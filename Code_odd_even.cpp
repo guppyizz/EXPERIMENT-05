afb5c9ec70060f8953ece595a93bcac625138dad
Updesh arora <updesh.arora4200@gmail.com>
2025-07-27 20:49:38 +0530

    Create Code_odd_even.cpp

---
+++ Code_odd_even.cpp
@@ -0,0 +1,23 @@
+//UPDESH ARORA
+// PRN - 24070123124
+// ENTC B-3
+// EXPERIMENT 05
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 1) Using if-else statement to find Even and Odd number
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+
+    if (num == 0) {
+        cout << "The number is ZERO..." << endl;
+    } else if (num % 2 == 0) {
+        cout << "The number is even" << endl;
+    } else {
+        cout << "The number is odd" << endl;
+    }
+
+   
